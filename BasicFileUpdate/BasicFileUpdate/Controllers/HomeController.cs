883b407a2c04983aaff1dc9b7ce2b031bf7a8f84
AvsarSuleyman <48950030+AvsarSuleyman@users.noreply.github.com>
2019-06-15 23:01:39 +0300

    Add files via upload

---
+++ BasicFileUpdate/BasicFileUpdate/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasicFileUpdate.Controllers
+{
+    public class HomeController : Controller
+    {
+       private string path;
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase[] files,int id)
+        {
+            for (int i = 0; i < files.Length; i++)
+            {
+                    var folder = Server.MapPath("~/upload");
+                    var randomfilename = Path.GetRandomFileName();
+                    var filename = Path.ChangeExtension(randomfilename, ".jpg");
+                    var path = Path.Combine(folder, filename);
+
+                    files[i].SaveAs(path);
+            }   
+            return Json("");
+        }
+    }
+}
---
+++ BasicFileUpdate/BasicFileUpdate/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BasicFileUpdate.MvcApplication" Language="C#" %>
